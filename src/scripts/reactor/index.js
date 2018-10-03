4953c8513000a0b80676cde24398ec12b4f69d2d
Samuil Georgiev <sami.georgiev@gmail.com>
2018-10-03 22:17:13 +0300

    improved added to ul

--- src/scripts/reactor/index.js
+++ src/scripts/reactor/index.js
@@ -191,27 +191,14 @@ function removeNeutron(n) {
 function updateStats() {
     ncount = document.getElementById("ncount");
     ncount.innerHTML = "Neutron count: " + neutrons.length;
-    document.getElementById("params").innerHTML =
-    "<li>ctrlRodChance<//li>" + CONFIG.controlRodChance +
-    "<li>ctrlRodCool<//li>" + CONFIG.controlRodCool +
-    "<li>ctrlRodHeat<//li>" + CONFIG.controlRodHeat +
-    "<li style='padding-top: 10px'>fuelChance<//li>" + CONFIG.fuelChance +
-    "<li>fuelCool<//li>" + CONFIG.fuelCool +
-    "<li>fuelHeat<//li>" + CONFIG.fuelHeat +
-    "<li>fuelSpontHeat<//li>" + CONFIG.fuelSpontHeat +
-    "<li>fuelSpontChance<//li>" + CONFIG.fuelSpontChance +
-    "<li style='padding-top: 10px'>heatMax<//li>" + CONFIG.heatMax +
-    "<li>heatTransfer<//li>" + CONFIG.heatTransfer +
-    "<li style='padding-top: 10px'>moderatorCool<//li>" + CONFIG.moderatorCool +
-    "<li style='padding-top: 10px'>nCardDir<//li>" + CONFIG.nCardDir +
-    "<li>nSpawnMin<//li>" + CONFIG.nSpawnMin +
-    "<li>nSpawnMax<//li>" + CONFIG.nSpawnMax +
-    "<li>nSpeedMin<//li>" + CONFIG.nSpeedMin +
-    "<li>nSpeedMax<//li>" + CONFIG.nSpeedMax +
-    "<li style='padding-top: 10px'>reflectorCool<//li>" + CONFIG.reflectorCool +
-    "<li>reflectorHeat<//li>" + CONFIG.reflectorHeat +
-    "<li style='padding-top: 10px'>renderGlow<//li>" + CONFIG.renderGlow +
-    "<li>wallCool<//li>" + CONFIG.wallCool;
+    list = document.getElementById("params");
+    list.innerHTML="";
+    for (var param in CONFIG) {
+      newItem = document.createElement("li");
+      newItem.innerHTML = param + ": " + CONFIG[param];
+      newItem.setAttribute("class", "paramItem");
+      list.appendChild(newItem);
+    }
 }
 
 
@@ -261,6 +248,6 @@ function draw() {
             neutrons[i].display();
         }
     }
-    
+
     updateStats();
 }
