df9c6d540f216c2eaa89ad04726bb9da2ec09dca
Victor Velev <velev.victor@yahoo.com>
2018-10-03 23:23:24 +0300

    [Update] New file structure

--- src/scripts/Simulation.js
+++ src/scripts/Simulation.js
@@ -1,7 +1,3 @@
-//////////////////////////
-//  Global variables    //
-//////////////////////////
-
 // Configuration
 const CONFIG = {
     controlRodChance: 50,   // chance for control rod to absorb a neutron
@@ -187,60 +183,4 @@ class Simulation {
     
         this.updateStats();
     }
-}
-
-//////////////////////////
-//   Utility functions  //
-//////////////////////////
-
-
-// Random choice from array
-function choose(choices) {
-    var index = floor(random() * choices.length);
-    return choices[index];
-}
-
-// Find the nearest tile
-function currentTile(x, y) {
-    return {
-        x: floor(x / RENDER.cellSize),
-        y: floor(y / RENDER.cellSize)
-    };
-}
-
-// Returns 1 or -1
-function plusOrMinus() {
-    return round(random()) * 2 - 1;
-}
-
-// Returns random neutron velocity
-function randVelocity() {
-    return random(CONFIG.nSpeedMin, CONFIG.nSpeedMax) * plusOrMinus();
-}
-
-
-//////////////////////////////////
-//  p5.js built-in functions    //
-//////////////////////////////////
-
-var sim = null;
-
-function setup() {
-    params = document.getElementById("params");
-    params.innerHTML = "";
-    
-    for (var param in CONFIG) {
-      newItem = document.createElement("li");
-      newItem.innerHTML = param + ": " + CONFIG[param];
-      newItem.setAttribute("class", "param");
-      params.appendChild(newItem);
-    }
-
-    sim = new Simulation(visualize=true)
-    genome = new Genome(sim)
-    sim.createReactor(genome)
-}
-
-function draw() {
-    sim.update()
 }
