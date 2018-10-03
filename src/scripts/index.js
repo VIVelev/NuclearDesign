de3c65996da22e65fc97e2f901a30924dc90414e
Victor Velev <velev.victor@yahoo.com>
2018-10-03 23:29:45 +0300

    [Update] move utils to their separate file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,3 @@
-//////////////////////////
-//   Utility functions  //
-//////////////////////////
-
-
-// Random choice from array
-function choose(choices) {
-    var index = floor(random() * choices.length);
-    return choices[index];
-}
-
-// Find the nearest tile
-function currentTile(x, y) {
-    return {
-        x: floor(x / RENDER.cellSize),
-        y: floor(y / RENDER.cellSize)
-    };
-}
-
-// Returns 1 or -1
-function plusOrMinus() {
-    return round(random()) * 2 - 1;
-}
-
-// Returns random neutron velocity
-function randVelocity() {
-    return random(CONFIG.nSpeedMin, CONFIG.nSpeedMax) * plusOrMinus();
-}
-
-
 //////////////////////////////////
 //  p5.js built-in functions    //
 //////////////////////////////////
---
+++ src/scripts/utils.js
@@ -0,0 +1,27 @@
+//////////////////////////
+//   Utility functions  //
+//////////////////////////
+
+// Random choice from array
+function choose(choices) {
+    var index = floor(random() * choices.length);
+    return choices[index];
+}
+
+// Find the nearest tile
+function currentTile(x, y) {
+    return {
+        x: floor(x / RENDER.cellSize),
+        y: floor(y / RENDER.cellSize)
+    };
+}
+
+// Returns 1 or -1
+function plusOrMinus() {
+    return round(random()) * 2 - 1;
+}
+
+// Returns random neutron velocity
+function randVelocity() {
+    return random(CONFIG.nSpeedMin, CONFIG.nSpeedMax) * plusOrMinus();
+}
