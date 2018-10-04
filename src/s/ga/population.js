4c12555aa34c09a3e510f11fded998ada1274429
Victor Velev <velev.victor@yahoo.com>
2018-10-04 17:16:47 +0300

    [Fix] dir-1

--- src/s/ga/population.js
+++ src/s/ga/population.js
@@ -65,7 +65,7 @@ class Population {
         console.log(`******************** Generation ${this.generation} ********************`);
         console.log(`Mean Fitness: ${this.getMeanFitness()}`);
         console.log(`Best Fitness: ${this.getCurrentBestFitness()}`);
-        console.log(`Fitness Standard Deviation: ${this.getStandardDeviation()}`);
+        console.log(`Standard Deviation: ${this.getStandardDeviation()}`);
         console.log("\n");
     }
 
